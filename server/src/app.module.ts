b186a06284c6a3821870f4ff894118178053e26f
Thuantham <miok.namomok.namo@gmail.com>
2020-04-16 11:53:03 +0700

    Add GroupSchema , UserSchema and config database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppGateway } from './app.gateway';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://dbUser:dbUserPassword@parallelchat-lbofg.mongodb.net/parallelchat?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService, AppGateway],
 })
