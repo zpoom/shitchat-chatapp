b7d983eae9748f6a53897ade23fc7fdf4aaadd18
zpoom <poom007.sk135@gmail.com>
2020-04-19 16:13:15 +0700

    fix typo of key _id

--- server/src/group/group.interface.ts
+++ server/src/group/group.interface.ts
@@ -2,6 +2,6 @@ import * as mongoose from 'mongoose';
 import { IUser } from 'src/user/user.interface';
 export interface IGroup extends mongoose.Document {
     name: string,
-    members: [IUser['id']],
+    members: [IUser['_id']],
     messages: [{ userId: IUser['_id'], timestamp: Date, message: String }]
 }
