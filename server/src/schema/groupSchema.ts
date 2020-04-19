b715a1c91266219d6a03703137eb511a834ffff9
Thuantham <miok.namomok.namo@gmail.com>
2020-04-19 17:15:41 +0700

    use groupname and username as primary keys

--- server/src/schema/groupSchema.ts
+++ server/src/schema/groupSchema.ts
@@ -1,7 +1,7 @@
 import * as mongoose from 'mongoose';
 
 export const GroupSchema = new mongoose.Schema({
-  name: String,
-  members: [mongoose.Schema.Types.ObjectId],
-  messages: [{ userId: mongoose.Schema.Types.ObjectId, timestamp: Date, message: String }]
+  groupname: String,
+  members: [String],
+  messages: [{ username: String, timestamp: Date, message: String }]
 });
