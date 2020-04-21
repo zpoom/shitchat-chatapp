5012f61f2b2e343b19eedb640701473a09086b24
zpoom <poom007.sk135@gmail.com>
2020-04-21 15:03:23 +0700

    mock joingroup(not call service yet)

--- server/src/app.gateway.ts
+++ server/src/app.gateway.ts
@@ -1,18 +1,27 @@
 import { SubscribeMessage, WebSocketGateway, OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { Logger } from '@nestjs/common';
+import { UserService } from './user/user.service';
 
 @WebSocketGateway()
 export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 
   @WebSocketServer() server: Server;
   private logger: Logger = new Logger('AppGateway');
+  private userService: UserService;
 
   @SubscribeMessage('msgToServer')
   handleMessage(client: Socket, payload: string): void {
     this.server.emit('msgToClient', payload);
   }
 
+  @SubscribeMessage('join')
+  handleJoinGroup(client: Socket, payload: any): void {
+    // const res = this.userService.joinGroup(payload.groupname, payload.username);
+    const res = `Connected ${payload.username} ${payload.groupname}`;
+    this.server.emit('joined', res);
+
+  }
   afterInit(server: Server) {
     this.logger.log('init');
   }
