c521fe2d9a2b3a3991773ad6b0256bde5998a229
Thadchet <sogood_boss@hotmail.com>
2020-04-22 06:37:37 +0700

    import socker

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,16 @@ import { Button } from 'antd';
 import { Switch, Link, BrowserRouter , Route } from 'react-router-dom';
 import { Home } from './pages';
 import Lobby from './pages/Lobby';
+import io from 'socket.io-client';
 
 function App() {
+
+  const socket = io('http://localhost:8080');
+  socket.on('join', function(){});
+  socket.on('joined', function(){});
+  socket.on('leave group', function(){});
+  socket.on('exit', function(){});
+
   return (
     <div className="App">
       <BrowserRouter>
