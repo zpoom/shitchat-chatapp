bc89595675ef8eb55e20bdd062d82b238b2f3e2b
Strik3agle98 <can_saelo@hotmail.com>
2020-04-24 16:04:13 +0700

    create group api done

--- server/src/group/create-group.dto.ts
+++ server/src/group/create-group.dto.ts
@@ -2,6 +2,6 @@ import { IUser } from 'src/user/user.interface';
 
 export class CreateGroupDto {
   groupname: String;
-  members: [IUser['username']];
-  messages: [{ username: IUser['username'], timestamp: Date, message: String }];
+  members: Array<String>;
+  messages: Array<object>;
   }
