4531fec8b6b4cac7587a91889c9fa39a8d608ba8
zpoom <poom007.sk135@gmail.com>
2020-04-29 17:04:40 +0700

    sender in message component

--- client/src/components/Message/index.tsx
+++ client/src/components/Message/index.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Row } from 'antd';
 import './index.css';
 interface MessageProps {
     children: any;
     isMine: boolean;
+    sender: string;
 }
 
 
-export default ({ children, isMine = true }: MessageProps) => {
+export default ({ children, isMine = true, sender }: MessageProps) => {
     return (
-        <div className={`message-box ${isMine ? 'mine' : ''}`}>
-            {children}
-        </div>
+        <Fragment>
+            <p>{sender}</p>
+            <div className={`message-box ${isMine ? 'mine' : ''}`}>
+                {children}
+            </div>
+        </Fragment>
     )
 }
