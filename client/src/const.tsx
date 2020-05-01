0128409821945b233bdf407b38386f3d6439d01d
zpoom <poom007.sk135@gmail.com>
2020-05-01 17:23:46 +0700

    deploy server

--- client/src/const.tsx
+++ client/src/const.tsx
@@ -11,6 +11,6 @@ export const dummyGroup = [
     { name: 'A', id: '1' }, { name: 'B', id: '2' }, { name: 'C', id: '3' }, { name: 'D', id: '4' }, { name: 'E', id: '5' }
 ]
 
-export const dummyMessage = [ {message : "Hello", sender : "me"},{message : "Hi", sender : "me"},{message : "What the fuck", sender : "not mine"}]
+export const dummyMessage = [{ message: "Hello", sender: "me" }, { message: "Hi", sender: "me" }, { message: "What the fuck", sender: "not mine" }]
 
-export const apiEndpoint = 'http://localhost:8080'; 
+export const apiEndpoint = 'http://34.87.115.67:8080'; 
